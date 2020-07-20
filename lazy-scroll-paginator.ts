5efff9ba49f679e3fd5885166eb3b4400863658a
Viktor Chernodub <37013688+chernodub@users.noreply.github.com>
2020-07-21 06:06:02 +0700

    Update lazy-scroll-paginator.ts

--- lazy-scroll-paginator.ts
+++ lazy-scroll-paginator.ts
@@ -1,46 +1,79 @@
+import {
+  Observable,
+  ReplaySubject,
+  Subject,
+  Subscription,
+  of,
+  combineLatest,
+  merge,
+  fromEvent,
+  EMPTY,
+  concat,
+  forkJoin,
+  BehaviorSubject,
+  iif,
+  observable,
+} from 'rxjs';
+import {
+  map,
+  tap,
+  shareReplay,
+  switchMapTo,
+  first,
+  mapTo,
+  startWith,
+  filter,
+  skip,
+  withLatestFrom,
+  concatMap,
+  take,
+  scan,
+  debounceTime,
+  defaultIfEmpty,
+  switchMap,
+  pluck,
+} from 'rxjs/operators';
 
-interface LazyLoaderOptions {
-  /** Offset from the edge of a container to load more items. */
-  readonly loadMoreOffset: number;
-  /** Initial page to load. */
-  readonly initialPage: number;
+function filterNull<T>() {
+  return (observable: Observable<T | null>) =>
+    observable.pipe(
+      filter((data) => data != null),
+      map((data) => data as NonNullable<T>),
+    );
 }
 
+type PaginationDirection = 'next' | 'prev';
+// PROBLY I FCKD UP THE WHOLE THING NOT USING NUM OF PAGE
 /** Pagination model. */
-export interface Pagination<T> {
-  /** Number of elements. */
-  itemsCount: number;
-
-  /** Number of pages. */
-  pagesCount?: number;
-
-  /** Current page. */
-  page?: number;
-
-  /** Result. */
+export type Pagination<T = unknown> = {
+  pos: PaginationDirection;
+  /** Items. */
   items: T[];
-}
+  [K in PaginationDirection]: boolean;
+};
 
+type LoadDataFunction<Q, D> = (
+  page: number,
+  query: Q,
+) => Observable<Pagination<D>>;
 
-class TwoSideLazyLoader<T, D = never> {
+export class TwoSideLazyLoader<T, D = void> {
   /** Shows whether there are more items before. */
   public readonly haveMorePrevious$: Observable<boolean>;
   /** Shows if there are items after the initial page. */
   public readonly haveMoreNext$: Observable<boolean>;
   /** Lazy loaded items. */
-  public readonly items$: Observable<T[]>;
+  public readonly items$: Observable<Pagination<T>[] | null>;
 
-  /** Emits number of page that is requested. */
-  private readonly pageRequested$: Observable<number>;
-  /** First page that should be loaded. */
-  private readonly firstLoadedPage$ = new ReplaySubject<number>(1);
-  /** Last loaded page of data. */
-  private readonly lastLoadedData$ = new Subject<Pagination<T>>();
+  /** Next that should be loaded. */
+  private readonly initialPage$: BehaviorSubject<number>;
+  /** Page of data. */
+  private readonly lastLoadedPage$: ReplaySubject<Pagination<T>>;
   /** Emitted when need to load data either from the start or end of list. */
-  private readonly load$ = new Subject<'previous' | 'next'>();
+  private readonly load$: Subject<PaginationDirection>;
 
   /** Subscription on container scroll events. */
-  private containerScrollSubscription: Subscription;
+  private containerScrollSubscription!: Subscription;
 
   /**
    * @constructor
@@ -49,109 +82,143 @@ class TwoSideLazyLoader<T, D = never> {
    * @param options Pagination options.
    */
   public constructor(
-    loadData: (page: number, query: D) => Observable<Pagination<T>>,
-    query$: Observable<D> = of(null),
-    private readonly options: LazyLoaderOptions = {
-      loadMoreOffset: 100,
-      initialPage: 0,
-    },
+    loadData: LoadDataFunction<D, Pagination<T>>,
+    initialPage: number,
+    query$: Observable<D> = EMPTY,
   ) {
+    this.initialPage$ = new BehaviorSubject(initialPage);
+    this.lastLoadedPage$ = new ReplaySubject(1);
+    this.load$ = new Subject();
+
     const queryChange$ = query$.pipe(
-      tap(() => this.firstLoadedPage$.next(options.initialPage)),
+      defaultIfEmpty(),
       shareReplay({ refCount: true, bufferSize: 1 }),
     );
 
-    this.haveMoreNext$ = queryChange$.pipe(
-      switchMapTo(this.lastLoadedData$.pipe(
-        map(({ page, pagesCount }) => pagesCount === 0 || page >= pagesCount - 1),
-        first(endLoaded => endLoaded),
-        mapTo(false), // Mark there is no more at bottom
-        startWith(true),
-      )),
+    this.haveMoreNext$ = this.initMoreItemsStream(queryChange$, 'next');
+    this.haveMorePrevious$ = this.initMoreItemsStream(queryChange$, 'prev');
+
+    const pageRequested$ = this.initRequestedPageStream(queryChange$);
+    this.items$ = this.initDataAccumulationStream(
+      queryChange$,
+      pageRequested$,
+      loadData,
+    );
+  }
+
+  private initMoreItemsStream(
+    queryChange$: Observable<D>,
+    direction: PaginationDirection,
+  ): Observable<boolean> {
+    return queryChange$.pipe(
+      switchMapTo(
+        this.lastLoadedPage$.pipe(
+          pluck(direction),
+          first((hasMore) => !hasMore),
+          mapTo(false), // Mark there is no more at top
+          startWith(true),
+        ),
+      ),
       shareReplay({ refCount: true, bufferSize: 1 }),
     );
+  }
 
-    this.haveMorePrevious$ = queryChange$.pipe(
-      switchMapTo(this.lastLoadedData$.pipe(
-        map(({ page }) => page === 0),
-        first(endLoaded => endLoaded),
-        mapTo(false), // Mark there is no more at top
-        startWith(true),
-      )),
-      shareReplay({ refCount: true, bufferSize: 1 }),
+  private initRequestedPageStream(
+    queryChange$: Observable<D>,
+  ): Observable<number> {
+    const loadPrev$: Observable<void> = this.load$.pipe(
+      filter((load) => load === 'next'),
+      mapTo(void 0),
     );
 
-    const loadPrev$ = this.load$.pipe(
-      filter(load => load === 'previous'), mapTo(null),
-    );
-
-    const loadNext$ = this.load$.pipe(
-      filter(load => load === 'next'), mapTo(null),
+    const loadNext$: Observable<void> = this.load$.pipe(
+      filter((load) => load === 'prev'),
+      mapTo(void 0),
     );
 
     const bottomPagesOffset$ = queryChange$.pipe(
-      switchMapTo(paginate(loadNext$).pipe(skip(1))), // Skip initial pagination request
+      switchMapTo(paginate(loadNext$)),
       withLatestFrom(this.haveMoreNext$),
       filter(([_, haveMore]) => haveMore),
       map(([offset]) => offset),
     );
 
     const topPagesOffset$ = queryChange$.pipe(
-      switchMapTo(paginate(loadPrev$).pipe(skip(1))), // Skip initial pagination request
+      switchMapTo(paginate(loadPrev$)),
       withLatestFrom(this.haveMorePrevious$),
       filter(([_, haveMore]) => haveMore),
-      map(([offset]) => -offset), // To have backward offset
+      map(([offset]) => -offset), // To have backward offset *(-1)
     );
 
-    const pageInit$ = this.firstLoadedPage$.pipe(filter(firstPageToLoad => firstPageToLoad != null));
-
-    this.pageRequested$ = pageInit$.pipe(
-      switchMapTo(combineLatest([
-        merge(bottomPagesOffset$, topPagesOffset$),
-        pageInit$,
-      ])),
+    return combineLatest([
+      merge(bottomPagesOffset$, topPagesOffset$),
+      this.initialPage$.pipe(filterNull()),
+    ]).pipe(
       map(([offset, firstPageToLoad]) => firstPageToLoad + offset),
-      startWith(options.initialPage),
+      startWith(this.initialPage$.value),
+    );
+  }
+
+  private initDataAccumulationStream(
+    queryChange$: Observable<D>,
+    pageChange$: Observable<number>,
+    loadData: LoadDataFunction<D, Pagination<T>>,
+  ): Observable<Pagination<T>[] | null> {
+    const lastLoaded$ = this.lastLoadedPage$.pipe(startWith(null));
+    const accumulateData$ = pageChange$.pipe(
+      withLatestFrom(queryChange$),
+      concatMap(([page, query]) => loadData(page, query).pipe(take(1))),
+      withLatestFrom(lastLoaded$),
+      // Cache loaded data
+      // tap(([nextPage]) => {
+      //   this.lastLoadedPage$.next(nextPage);
+      //   if (this.initialPage$.value == null) {
+      //     // In case the initial page is not passed, take the first loaded
+      //     this.initialPage$.next(nextPage.page);
+      //   }
+      // }),
+      scan(
+        (
+          loadedItems: Pagination<T>[],
+          [nextPage, prevPage]: [Pagination<T>, Pagination<T> | null],
+        ) => {
+          if (this.initialPage$.value === nextPage.page) {
+            return nextPage.items;
+          }
+          if (nextPage.page < prevPage.page) {
+            return nextPage.items.concat(loadedItems);
+          }
+          return loadedItems.concat(nextPage);
+        },
+        [] as T[],
+      ),
+      startWith(null),
     );
 
-    this.items$ = queryChange$.pipe(
-      switchMapTo(this.pageRequested$.pipe(
-        withLatestFrom(queryChange$),
-        concatMap(([page, query]) => loadData(page, query).pipe(take(1))),
-        withLatestFrom(this.firstLoadedPage$),
-        scan((loadedItems: T[], [dataPage, firstLoadedPage]: [Pagination<T>, number]) => {
-          this.lastLoadedData$.next(dataPage);
-          if (firstLoadedPage == null || dataPage.page === firstLoadedPage) {
-            // In case the initial page is not passed, take the first loaded
-            this.firstLoadedPage$.next(dataPage.page);
-            return dataPage.items;
-          }
-          if (dataPage.page < firstLoadedPage) {
-            return dataPage.items.concat(loadedItems);
-          }
-          return loadedItems.concat(dataPage.items);
-        }, [] as T[]),
-        startWith(null),
-      )),
-    );
+    return queryChange$.pipe(switchMapTo(accumulateData$));
   }
 
   /**
    * Handle scrolling events on pagination container.
    * @param element Pagination container.
    */
-  public attachTo(element: HTMLElement, scrollDebounceTime: number = 300): void {
-    const handleScroll$ = merge(
-      fromEvent(element, 'scroll'),
-      this.items$,
-    ).pipe(
+  public attachTo(
+    element: HTMLElement,
+    scrollDebounceTime: number = 300,
+    loaderOffsetPx: number = 100,
+  ): void {
+    const handleScroll$ = merge(fromEvent(element, 'scroll'), this.items$).pipe(
+      tap((data) => console.log(data)),
       debounceTime(scrollDebounceTime),
       withLatestFrom(this.haveMorePrevious$, this.haveMoreNext$),
       tap(([_, morePrev, moreNext]) => {
-        if (this.isElementScrolledToTop(element) && morePrev) {
-          element.scrollTo({ top: this.options.loadMoreOffset });
-          this.load$.next('previous');
-        } else if (this.isElementScrolledToBottom(element) && moreNext) {
+        if (this.isElementScrolledToTop(element, loaderOffsetPx) && morePrev) {
+          element.scrollTo({ top: loaderOffsetPx });
+          this.load$.next('prev');
+        } else if (
+          this.isElementScrolledToBottom(element, loaderOffsetPx) &&
+          moreNext
+        ) {
           this.load$.next('next');
         }
       }),
@@ -164,51 +231,40 @@ class TwoSideLazyLoader<T, D = never> {
     this.containerScrollSubscription.unsubscribe();
   }
 
-  private isElementScrolledToTop(elem: HTMLElement): boolean {
-    return elem.scrollTop < this.options.loadMoreOffset;
+  private isElementScrolledToTop(elem: HTMLElement, offset: number): boolean {
+    return elem.scrollTop < offset;
   }
 
-  private isElementScrolledToBottom(elem: HTMLElement): boolean {
-    const topOffset = elem.offsetHeight + this.options.loadMoreOffset;
-    return elem.scrollTop >= (elem.scrollHeight - topOffset);
+  private isElementScrolledToBottom(
+    elem: HTMLElement,
+    offset: number,
+  ): boolean {
+    const topOffset = elem.offsetHeight + offset;
+    return elem.scrollTop >= elem.scrollHeight - topOffset;
   }
 }
 
 /**
  * Pagination helper.
- * @param nextPageRequested$ Hot observable that emits when page is requested.
- * @param pageReset$ Observable that emits when we want to reset pagination to 0.
- * @returns Page change observable.
- * @example
- *
- * const wantMore$ = new Subject<void>();
- * const queryChange$ = new Subject<void>();
- *
- * const paginationData$ = paginate(
- *    wantMore$,
- *    queryChange$,
- *  ).pipe(
- *    withLatestFrom(queryChange$),
- *    switchMap(([page, query]) =>
- *      someService.searchForSomething({ query, page })),
- *  );
- *
- * paginationData$.subscribe(console.log);
- *
- * queryChange$.next(); // prints data on 0 page
- * wantMore$.next(); // prints data on 1st page
- * queryChange.next(); // prints data on 0 page
+ * TODO
  */
 export function paginate(
+  this: void,
   nextPageRequested$: Observable<void>,
-  pageReset$: Observable<void> = of(null),
+  pageReset$?: Observable<void>,
+  startWith$?: Observable<number>,
 ): Observable<number> {
-  const pageAccumulation$ = nextPageRequested$.pipe(
-    mapTo(1), // Set number of requested pages on every emit
-    startWith(0), // Set initial page
-    scan(((curPage: number, requestedPages: 1) => curPage + requestedPages)),
+  const pageAccumulation$ = concat(
+    startWith$ ? startWith$ : EMPTY,
+    nextPageRequested$.pipe(
+      mapTo(1), // Set number of requested pages on every emit
+      scan(
+        (curPage: number, requestedPages: number) => curPage + requestedPages,
+      ),
+    ),
   );
-  return pageReset$.pipe(
-    switchMapTo(pageAccumulation$),
-  );
+
+  return pageReset$
+    ? pageReset$.pipe(switchMapTo(pageAccumulation$))
+    : pageAccumulation$;
 }
